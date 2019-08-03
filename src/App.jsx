d558c24df2d1d47d88e0fd0db1cf329f90b65b5b
jennizalwango <jennyzalwango12@gmail.com>
2019-08-03 23:58:55 +0300

    add coverage report

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {toast} from 'react-toastify';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ToastsContainer, ToastsStore } from 'react-toasts';
+// import { ToastsContainer, ToastsStore } from 'react-toasts';
 import LoginContainer from './Login/containers/LoginContainer';
 import SignupContainer from './signup/Containers/SignupContainer';
 import MessageContainer from './fetchallMessges/Container/Messagecontainer';
+import 'react-toastify/dist/ReactToastify.css';
+
+toast.configure();
 export default function App () {
     return (
         <Router history={history}>
-            <ToastsContainer store={ToastsStore}/>
+            
             <Switch>
                 <Route path="/" exact component={LoginContainer} />
                 <Route path="/login" exact component={LoginContainer} />
